64b71b1b3c22a441fd9c5020daa674cb6ddcf559
Solomon Ritzow <11698787+ritzow@users.noreply.github.com>
2021-09-09 20:49:48 -0700

    Update init

--- sl-src/main.c
+++ sl-src/main.c
@@ -1,6 +1,15 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 
 void _start() {
+	int in = open("/dev/console", O_APPEND);
+
 	while(1) {
 		/* Busy wait */
+		write(in, "hello\n", sizeof "hello\n");
+		sleep(1);
 	}
 }
